e7e5cd9c8bfd1bea3914c563cc7679083dd20d5a
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-10 17:07:08 -0500

    new user form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation, :username
 
+  validates :email, :name, :password, :password_confirmation, :presence => :true
   has_secure_password
 
   has_many :reviews
