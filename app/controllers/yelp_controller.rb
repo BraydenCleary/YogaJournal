160bef859bc3504ec88e67c2364b7907655c2e08
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-10 16:59:44 -0500

    finished yelp api

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -1,6 +1,11 @@
 class YelpController < ApplicationController
 	def index
 
+		require 'rubygems'
+		require 'oauth'
+		require 'JSON'
+
+
 		consumer_key = '8ucfXv9o4bepqvDLD-COhA'
 		consumer_secret = '3ZZTWzuUG8CfKtBzJyxOO4ZS8z8'
 		token = 'aYZ1-B8K3cxie8Jq8kJItqmZ0X1F39Jy'
@@ -11,10 +16,11 @@ class YelpController < ApplicationController
 		consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
 		access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
-		path = "/v2/search?term=#{params['yoga']}&location=#{params['zipcode']}"
+		path = "/v2/search?term=Yoga&location=#{params['zipcode']}"
 
-		@results = access_token.get(path).body
+		master_hash = access_token.get(path)
 
+		@response = JSON.parse(master_hash.body)
 	end
 
 end
--- app/models/meditation_post.rb
+++ app/models/meditation_post.rb
@@ -1,5 +1,5 @@
 class MeditationPost < ActiveRecord::Base
-  attr_accessible :breath, :feeling, :length, :user_id, date
+  attr_accessible :breath, :feeling, :length, :user_id, :date
 
   belongs_to :user
 end
